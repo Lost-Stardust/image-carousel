e0aed7da760652b10e6ca368a419d9216f37cf7d
isa <isaismail3000@gmail.com>
2024-07-06 18:57:15 +0300

    Cycle through images on "next" arrow click

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 import "./styles.css";
 console.log("It's alive!!");
+
+const imgArr = document.querySelectorAll("img");
+console.log(imgArr);
+
+let imgNum = 0;
+const next = document.querySelector(".next");
+
+// on button click, cycle to next image using imgNum as index for imgArr
+next.addEventListener("click", () => {
+  console.log(imgArr.length);
+  if (imgNum === imgArr.length - 1) {
+    imgNum = -1;
+    console.log(imgNum);
+  }
+  imgNum++;
+  imgArr[imgNum].scrollIntoView();
+});
